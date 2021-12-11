6abb6dc36ad727f8b7bb4bc9861051152f7f5b36
Cosmin Stoian <cosmin@bceng.tech>
2021-12-11 13:15:45 +0200

    Configuring code style

--- src/install.js
+++ src/install.js
@@ -1,16 +1,16 @@
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 const HelloWorldSimple = {
-    install(Vue) {
-        // Let's register our component globally
-        // https://vuejs.org/v2/guide/components-registration.html
-        Vue.component('hello-world', HelloWorld)
-    }
-}
+  install(Vue) {
+    // Let's register our component globally
+    // https://vuejs.org/v2/guide/components-registration.html
+    Vue.component("hello-world", HelloWorld);
+  },
+};
 
 // Automatic installation if Vue has been added to the global scope.
-if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(HelloWorldSimple)
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.use(HelloWorldSimple)
 }
 
-export default HelloWorldSimple
+export default HelloWorldSimple;
