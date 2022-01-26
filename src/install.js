0d112baa567f9a6a80038cfb6dce5d85edc260b4
Cosmin Stoian <cosmyn9708@gmail.com>
2022-01-26 15:50:08 +0200

    Removed automatic installation script from install.js

--- src/install.js
+++ src/install.js
@@ -14,9 +14,4 @@ const SchematiqueSimple = {
   },
 };
 
-// Automatic installation if Vue has been added to the global scope.
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(SchematiqueSimple);
-}
-
 export default SchematiqueSimple;
