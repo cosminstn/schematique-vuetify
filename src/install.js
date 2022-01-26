51f40f05020a2841f3a0b517c6b02b58e43dff6b
Cosmin Stoian <cosmyn9708@gmail.com>
2022-01-26 11:32:43 +0200

    Manually registered date and text widgets in src/install.js

--- src/install.js
+++ src/install.js
@@ -1,3 +1,5 @@
+import DateWidget from './components/widgets/DateWidget.vue';
+import TextWidget from './components/widgets/TextWidget.vue';
 import SchematiqueForm from './components/SchematiqueForm.vue';
 import SchematiqueWidget from './components/SchematiqueWidget.vue';
 
@@ -5,6 +7,8 @@ const SchematiqueSimple = {
   install(Vue) {
     // Let's register our component globally
     // https://vuejs.org/v2/guide/components-registration.html
+    Vue.component('date-widget', DateWidget);
+    Vue.component('text-widget', TextWidget);
     Vue.component('schematique-form', SchematiqueForm);
     Vue.component('schematique-widget', SchematiqueWidget);
   },
